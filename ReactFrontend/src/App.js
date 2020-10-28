e82d01fece9a6fe285b91cb01d783add43510423
LeeYoder <lwyoder807@gmail.com>
2020-10-28 13:22:06 -0500

    added two types of dropdown boxes.

--- ReactFrontend/src/App.js
+++ ReactFrontend/src/App.js
@@ -2,8 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import RestuarantExample from './components/RestuarantDisplayExample';
+import Dropdown from './Components/Dropdown';
+
+const container = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '200vw',
+  height: '10vh',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+const options = [
+  { key: 'key-1', text: 'All' },
+  { key: 'key-2', text: 'Chinese' }
+]
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +26,17 @@ function App() {
         <p>
           Example code:
         </p>
+
+		<div style={container}>
+      		Example Dropdown <Dropdown options={options} />
+    	</div>
+
+<select id="test" name="test">
+     <option value="test1">All</option>
+     <option value="test2">Restaurant</option>
+     <option value="Other">Other</option>
+</select>
+
 		<div>
 			<RestuarantExample/>
 		</div>
